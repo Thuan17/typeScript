01ecfee47292d9a101272beb18e93cb4c5c4c794
Thuan17 <thuanhuynh941.com>
2023-08-21 22:44:12 +0700

    TypeScript

--- TypeScript/TuHoc/project/#TongHop/TongHop.ts
+++ TypeScript/TuHoc/project/#TongHop/TongHop.ts
@@ -1,33 +1,55 @@
-/// kie u string 
-let ten:string  =("Thuan ");
-let ten01=`Huynh ${ten}`;
+// / kie u string 
+// let ten:string  =("Thuan ");
+// let ten01=`Huynh ${ten}`;
 
-console.log("Kieu Tring ",ten01);
-
-
-//boolean
-let sta:boolean=true;
-let pro11:boolean =Boolean("Huynh Thuan")
-console.log("Kieu boolean ",pro11);
+// console.log("Kieu Tring ",ten01);
 
 
-//object 
+// //boolean
+// let sta:boolean=true;
+// let pro11:boolean =Boolean("Huynh Thuan")
+// console.log("Kieu boolean ",pro11);
 
-let inforr:{
-    name:string,
-    age:number,
-    location:string
+
+// //object 
+
+// let inforr:{
+//     name:string,
+//     age:number,
+//     location:string
+// }={
+//     name:"Thuan",
+//     age:21,
+//     location:'KyThuat',
+// }
+
+// console.log("Kieu object ",inforr);
+
+// // arrayType
+
+// let arryy:(string | number)[]=["Thuan"]
+// arryy.push(25);
+
+// console.log("Kieu object ",arryy);
+
+
+
+
+
+
+
+let Namee:string=("thhuan");
+let nameeeee =`Huynh Gia ${Namee}`;
+console.log("New String",nameeeee);
+
+
+let Thongtin:{
+    ten:string,
+    tuoi:number,
+    gender:string
 }={
-    name:"Thuan",
-    age:21,
-    location:'KyThuat',
+    ten:'Thuan',
+    tuoi:21,
+    gender:'nam'
 }
-
-console.log("Kieu object ",inforr);
-
-//arrayType
-
-let arryy:(string | number)[]=["Thuan"]
-arryy.push(25);
-
-console.log("Kieu object ",arryy);
+console.log("Object",Thongtin)
